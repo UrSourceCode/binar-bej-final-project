2b3806fdfda1d047d777261ed62ac65ac42a9870
Agam33 <riswanagam@gmail.com>
2022-11-24 09:35:08 +0700

    #disable spring security

---
+++ src/main/java/com/binar/flyket/dto/response/Response.java
@@ -0,0 +1,25 @@
+package com.binar.flyket.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Response<T>{
+    private Integer statusCode;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date timeStamp;
+    private String message;
+    private T data;
+}
