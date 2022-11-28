436286111306b00fc4b0f036a40019ef86999ae5
Agam33 <riswanagam@gmail.com>
2022-11-28 21:26:49 +0700

    #update

--- src/test/java/com/binar/flyket/FlyketApplicationTests.java
+++ src/test/java/com/binar/flyket/FlyketApplicationTests.java
@@ -1,11 +1,18 @@
 package com.binar.flyket;
 
-import com.binar.flyket.dto.model.AirportDetailDTO;
+import 	com.binar.flyket.dto.model.AirportDetailDTO;
 import com.binar.flyket.repository.AirportRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +24,5 @@ class FlyketApplicationTests {
 	private AirportRepository repository;
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 }
