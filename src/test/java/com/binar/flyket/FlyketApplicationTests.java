b858f9790071b81d5db5afa2ac5e9dc2995bf7cc
Agam33 <riswanagam@gmail.com>
2022-11-28 21:27:47 +0700

    delete test

--- src/test/java/com/binar/flyket/FlyketApplicationTests.java
+++ src/test/java/com/binar/flyket/FlyketApplicationTests.java
@@ -1,20 +1,9 @@
 package com.binar.flyket;
 
-import 	com.binar.flyket.dto.model.AirportDetailDTO;
 import com.binar.flyket.repository.AirportRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 class FlyketApplicationTests {
