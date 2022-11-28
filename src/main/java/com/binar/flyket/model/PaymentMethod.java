97e0daa33e57b384619cb3679f15631111aae340
Agam33 <riswanagam@gmail.com>
2022-11-29 00:17:42 +0700

    #update

--- src/main/java/com/binar/flyket/model/PaymentMethod.java
+++ src/main/java/com/binar/flyket/model/PaymentMethod.java
@@ -1,5 +1,6 @@
 package com.binar.flyket.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class PaymentMethod {
     @Column(name = "name")
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "paymentMethod", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookingList = new ArrayList<>();
 }
