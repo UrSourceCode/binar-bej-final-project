1a8f90f785ba68b42735d8b6a1450cb292706214
Agam33 <riswanagam@gmail.com>
2022-11-29 13:41:35 +0700

    Schedule: controller

--- src/main/java/com/binar/flyket/controller/FlightScheduleController.java
+++ src/main/java/com/binar/flyket/controller/FlightScheduleController.java
@@ -1,8 +1,15 @@
 package com.binar.flyket.controller;
 
+import com.binar.flyket.dto.response.Response;
+import com.binar.flyket.dto.response.ResponseError;
+import com.binar.flyket.exception.FlyketException;
 import com.binar.flyket.service.FlightScheduleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.binar.flyket.utils.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/flight/schedules")
@@ -13,4 +20,38 @@ public class FlightScheduleController {
     public FlightScheduleController(FlightScheduleService flightScheduleService) {
         this.flightScheduleService = flightScheduleService;
     }
+
+
+    @GetMapping
+    public ResponseEntity<?> getFlightSchedules() {
+        return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                Constants.SUCCESS_MSG,
+                flightScheduleService.getFlightScheduleDetails()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getFlightSchedulesById(@PathVariable("id") String id) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(), Constants.SUCCESS_MSG,
+                    flightScheduleService.getFlightScheduleDetailById(id)));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(),
+                    new Date(), e.getMessage()), e.getStatusCode());
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteScheduleById(@PathVariable("id") String id) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(), Constants.SUCCESS_MSG,
+                    flightScheduleService.deleteFlightScheduleById(id)));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(),
+                    new Date(), e.getMessage()), e.getStatusCode());
+        }
+    }
+
+
+    // TODO: udpate
 }
--- src/main/java/com/binar/flyket/repository/query/FlightScheduleQuery.java
+++ src/main/java/com/binar/flyket/repository/query/FlightScheduleQuery.java
@@ -18,11 +18,11 @@ public class FlightScheduleQuery {
 
     public static final String FLIGHT_SCHEDULE_DETAIL_JOIN_BY_ID = "SELECT " +
             "NEW com.binar.flyket.dto.model.FlightScheduleDetailDTO(fs.id, " +
-            "fs.departureTime, fs.arrivalTime, fs.flightDate, acd.aircraftClass, " +
-            "acd.aircraft.type, " +
-            "fr.fromAirport.IATACode, fr.fromAirport.name, fr.fromAirport.city, " +
-            "fr.toAirport.IATACode, fr.toAirport.name, fr.toAirport.city, " +
-            "fr.hours, fr.minutes, acd.price, acd.maxBaggage, acd.maxCabin) " +
+                "fs.departureTime, fs.arrivalTime, fs.flightDate, acd.aircraftClass, " +
+                "acd.aircraft.type, " +
+                "fr.fromAirport.IATACode, fr.fromAirport.name, fr.fromAirport.city, " +
+                "fr.toAirport.IATACode, fr.toAirport.name, fr.toAirport.city, " +
+                "fr.hours, fr.minutes, acd.price, acd.maxBaggage, acd.maxCabin) " +
             "FROM FlightSchedule AS fs " +
             "JOIN fs.aircraftDetail AS acd " +
             "JOIN fs.flightRoute AS fr " +
