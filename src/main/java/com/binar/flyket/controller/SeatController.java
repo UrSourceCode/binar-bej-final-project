acc8eb5930bf0a547b4dfcee6e0f17f85626af89
gianzra <gianzra1@gmail.com>
2022-11-30 19:55:44 +0700

    add SeatController

---
+++ src/main/java/com/binar/flyket/controller/SeatController.java
@@ -0,0 +1,71 @@
+package com.binar.flyket.controller;
+
+import com.binar.flyket.dto.model.SeatDTO;
+import com.binar.flyket.dto.response.Response;
+import com.binar.flyket.dto.response.ResponseError;
+import com.binar.flyket.exception.FlyketException;
+import com.binar.flyket.service.SeatService;
+import com.binar.flyket.utils.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/seats")
+public class SeatController {
+
+    private final SeatService seatService;
+
+    public SeatController(SeatService seatService) {
+        this.seatService = seatService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getSeats() {
+        return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                Constants.SUCCESS_MSG, seatService.getSeats()));
+    }
+
+    @PostMapping("/addAll")
+    public ResponseEntity<?> addSeats(@RequestBody List<SeatDTO> seats) {
+        return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                Constants.SUCCESS_MSG, seatService.addSeats(seats)));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addSeat(@RequestBody SeatDTO seat) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                    Constants.SUCCESS_MSG, seatService.addSeat(seat)));
+        } catch (FlyketException.DuplicateEntityException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(),
+                    new Date(),e.getMessage()), e.getStatusCode());
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteSeat(@RequestParam("id") Integer seatId) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                    Constants.SUCCESS_MSG, seatService.deleteSeat(seatId)));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(),
+                    new Date(),e.getMessage()), e.getStatusCode());
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> getSeatById(@RequestParam("id") Integer seatId) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
+                    Constants.SUCCESS_MSG, seatService.getSeatById(seatId)));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(),
+                    new Date(),e.getMessage()), e.getStatusCode());
+        }
+    }
+
+}
