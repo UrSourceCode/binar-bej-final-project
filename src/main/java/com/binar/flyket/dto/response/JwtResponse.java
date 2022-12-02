88a857657708c8c58c2ba8712dd7e655e7bcbd56
Agam33 <riswanagam@gmail.com>
2022-12-02 12:49:42 +0700

    Update JwtResponse.java

--- src/main/java/com/binar/flyket/dto/response/JwtResponse.java
+++ src/main/java/com/binar/flyket/dto/response/JwtResponse.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class JwtResponse {
     private String email;
+    private Object role;
     private String token;
 }
