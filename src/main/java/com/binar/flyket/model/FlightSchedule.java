d8024bd4427117b95c0c334db63a786d79e690de
Agam33 <riswanagam@gmail.com>
2022-12-03 13:37:22 +0700

    Update FlightSchedule.java

--- src/main/java/com/binar/flyket/model/FlightSchedule.java
+++ src/main/java/com/binar/flyket/model/FlightSchedule.java
@@ -7,9 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @Getter
@@ -20,8 +17,12 @@ public class FlightSchedule {
 
     @Id
     private String id;
+
     private LocalDateTime departureTime;
+
     private LocalDateTime arrivalTime;
+
+    private LocalDate flightDate;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "aircraft_detail_id")
