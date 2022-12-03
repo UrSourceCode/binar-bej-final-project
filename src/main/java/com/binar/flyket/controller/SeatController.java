bdd0f71fc8279097c74763c70d1fcd5d10c644b4
Agam33 <riswanagam@gmail.com>
2022-12-03 16:14:06 +0700

    Update SeatController.java

--- src/main/java/com/binar/flyket/controller/SeatController.java
+++ src/main/java/com/binar/flyket/controller/SeatController.java
@@ -4,6 +4,7 @@ import com.binar.flyket.dto.model.SeatDTO;
 import com.binar.flyket.dto.response.Response;
 import com.binar.flyket.dto.response.ResponseError;
 import com.binar.flyket.exception.FlyketException;
+import com.binar.flyket.model.SeatNo;
 import com.binar.flyket.service.SeatService;
 import com.binar.flyket.utils.Constants;
 import org.springframework.http.HttpStatus;
@@ -47,7 +48,7 @@ public class SeatController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteSeat(@RequestParam("id") Integer seatId) {
+    public ResponseEntity<?> deleteSeat(@RequestBody SeatNo seatId) {
         try {
             return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
                     Constants.SUCCESS_MSG, seatService.deleteSeat(seatId)));
@@ -58,7 +59,7 @@ public class SeatController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<?> getSeatById(@RequestParam("id") Integer seatId) {
+    public ResponseEntity<?> getSeatById(@RequestBody SeatNo seatId) {
         try {
             return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(),
                     Constants.SUCCESS_MSG, seatService.getSeatById(seatId)));
