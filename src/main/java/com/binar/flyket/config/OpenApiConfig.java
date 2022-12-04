8cf11a6f9db6671d4524bad4e8e6b097231949e8
Agam33 <riswanagam@gmail.com>
2022-12-04 12:57:07 +0700

    Update OpenApiConfig.java

--- src/main/java/com/binar/flyket/config/OpenApiConfig.java
+++ src/main/java/com/binar/flyket/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +34,7 @@ public class OpenApiConfig {
                         new SecurityRequirement()
                                 .addList("bearer-jwt", Arrays.asList("read", "write"))
                                 .addList("bearer-key", Collections.emptyList())
-                );
+                )
+                .addServersItem(new Server().url("https://api-flyket.up.railway.app/"));
     }
 }
