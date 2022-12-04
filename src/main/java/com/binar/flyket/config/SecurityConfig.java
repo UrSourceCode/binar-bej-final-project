06100e4e741751ecc507f53750b944eaeaf49e49
Agam33 <riswanagam@gmail.com>
2022-12-04 15:53:50 +0700

    Update SecurityConfig.java

--- src/main/java/com/binar/flyket/config/SecurityConfig.java
+++ src/main/java/com/binar/flyket/config/SecurityConfig.java
@@ -12,12 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
