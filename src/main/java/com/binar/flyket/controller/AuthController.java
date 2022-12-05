bae79a25449b853cdc45be27ebe0d4e3d6f888e6
Agam33 <riswanagam@gmail.com>
2022-12-05 13:21:04 +0700

    Update AuthController.java

--- src/main/java/com/binar/flyket/controller/AuthController.java
+++ src/main/java/com/binar/flyket/controller/AuthController.java
@@ -17,15 +17,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@CrossOrigin(value = "*", maxAge = 3600L)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
