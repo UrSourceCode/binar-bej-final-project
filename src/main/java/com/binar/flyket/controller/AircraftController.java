2e8e47f6474659def22932cbac603b870ab66dad
Agam33 <riswanagam@gmail.com>
2022-12-05 13:20:55 +0700

    Update AircraftController.java

--- src/main/java/com/binar/flyket/controller/AircraftController.java
+++ src/main/java/com/binar/flyket/controller/AircraftController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(value = "*", maxAge = 3600L)
 @RestController
 @RequestMapping("/api/aircraft")
 public class AircraftController {
