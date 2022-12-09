7797ed06898083b0d46e108fa6182f689215cb24
Agam33 <riswanagam@gmail.com>
2022-12-09 19:21:15 +0700

    Create SeatDetailService.java

---
+++ src/main/java/com/binar/flyket/service/SeatDetailService.java
@@ -0,0 +1,13 @@
+package com.binar.flyket.service;
+
+
+import com.binar.flyket.dto.request.SeatRequest;
+
+
+public interface SeatDetailService {
+
+    Boolean addSeat(SeatRequest request);
+
+
+
+}
