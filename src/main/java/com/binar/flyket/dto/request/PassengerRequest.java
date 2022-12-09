8d46792378aa6de167aa77ee14aaf71aef5834e5
Agam33 <riswanagam@gmail.com>
2022-12-09 09:15:12 +0700

    Update PassengerRequest.java

--- src/main/java/com/binar/flyket/dto/request/PassengerRequest.java
+++ src/main/java/com/binar/flyket/dto/request/PassengerRequest.java
@@ -1,13 +1,17 @@
 package com.binar.flyket.dto.request;
 
+import com.binar.flyket.model.SeatNo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter@Getter@AllArgsConstructor@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PassengerRequest {
     private String title;
     private String name;
-    private SeatNoRequest seatNoRequest;
+    private SeatNo seatNo;
 }
