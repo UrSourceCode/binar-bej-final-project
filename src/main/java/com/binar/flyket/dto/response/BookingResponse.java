ae8368c0dd2a924f2a020f847901db13af73dc3c
Agam33 <riswanagam@gmail.com>
2022-12-09 09:15:14 +0700

    Update BookingResponse.java

--- src/main/java/com/binar/flyket/dto/response/BookingResponse.java
+++ src/main/java/com/binar/flyket/dto/response/BookingResponse.java
@@ -3,6 +3,7 @@ package com.binar.flyket.dto.response;
 import com.binar.flyket.model.BookingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,9 +11,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Setter
 @Getter
+@NoArgsConstructor
 public class BookingResponse {
     private String bookingId;
-    private String userName;
+    private String name;
     private String userEmail;
     private LocalDateTime date;
     private BookingStatus bookingStatus;
