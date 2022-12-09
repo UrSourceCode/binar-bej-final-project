aaef3fabadc5347fc91fa8d226b156161180b157
Agam33 <riswanagam@gmail.com>
2022-12-09 19:21:19 +0700

    Create SeatDetailServiceImpl.java

---
+++ src/main/java/com/binar/flyket/service/SeatDetailServiceImpl.java
@@ -0,0 +1,50 @@
+package com.binar.flyket.service;
+
+import com.binar.flyket.dto.request.SeatRequest;
+import com.binar.flyket.exception.ExceptionType;
+import com.binar.flyket.exception.FlyketException;
+import com.binar.flyket.model.AircraftDetail;
+import com.binar.flyket.model.SeatDetail;
+import com.binar.flyket.repository.AircraftDetailRepository;
+import com.binar.flyket.repository.SeatDetailRepository;
+import com.binar.flyket.utils.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SeatDetailServiceImpl implements SeatDetailService {
+
+    private SeatDetailRepository seatDetailRepository;
+
+    private AircraftDetailRepository aircraftDetailRepository;
+
+    public SeatDetailServiceImpl(SeatDetailRepository seatDetailRepository, AircraftDetailRepository aircraftDetailRepository) {
+        this.seatDetailRepository = seatDetailRepository;
+        this.aircraftDetailRepository = aircraftDetailRepository;
+    }
+
+    @Override
+    public Boolean addSeat(SeatRequest request) {
+        String seatId = request.getAircraftDetailId()+"-"+ request.getRow() + request.getNo();
+
+        Optional<SeatDetail> seatDetail = seatDetailRepository.findById(seatId);
+        if(seatDetail.isPresent())
+            throw FlyketException.throwException(ExceptionType.DUPLICATE_ENTITY, HttpStatus.CONFLICT, Constants.ALREADY_EXIST_MSG);
+
+        Optional<AircraftDetail> aircraftDetail = aircraftDetailRepository.findById(request.getAircraftDetailId());
+        if(aircraftDetail.isEmpty())
+            throw FlyketException.throwException(ExceptionType.NOT_FOUND, HttpStatus.NOT_FOUND, "Aircraft Detail " + Constants.NOT_FOUND_MSG);
+
+        SeatDetail seatDetailModel = new SeatDetail();
+        seatDetailModel.setId(seatId);
+        seatDetailModel.setAircraftDetail(aircraftDetail.get());
+        seatDetailModel.setStatus(false);
+        seatDetailModel.setNo(request.getNo());
+        seatDetailModel.setRow(request.getRow());
+
+        return true;
+    }
+
+}
