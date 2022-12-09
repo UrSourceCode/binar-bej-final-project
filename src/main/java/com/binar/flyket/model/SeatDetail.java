176443a6047e847db311c4d898783166dfd54349
Agam33 <riswanagam@gmail.com>
2022-12-09 19:21:03 +0700

    Update SeatDetail.java

--- src/main/java/com/binar/flyket/model/SeatDetail.java
+++ src/main/java/com/binar/flyket/model/SeatDetail.java
@@ -1,7 +1,9 @@
 package com.binar.flyket.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,12 +13,13 @@ import java.util.List;
 @Entity
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "seat_detail")
 public class SeatDetail {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private String id;
 
     private String row;
 
