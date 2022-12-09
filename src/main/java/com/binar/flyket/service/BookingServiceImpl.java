b8560feea6b053419e7adffae37fd16b4c024c2c
Agam33 <riswanagam@gmail.com>
2022-12-09 09:15:34 +0700

    Update BookingServiceImpl.java

--- src/main/java/com/binar/flyket/service/BookingServiceImpl.java
+++ src/main/java/com/binar/flyket/service/BookingServiceImpl.java
@@ -1,38 +1,39 @@
 package com.binar.flyket.service;
 
 import com.binar.flyket.dto.request.BookingRequest;
+import com.binar.flyket.dto.request.PassengerRequest;
 import com.binar.flyket.dto.response.BookingResponse;
 import com.binar.flyket.exception.FlyketException;
-import com.binar.flyket.model.AircraftDetail;
-import com.binar.flyket.model.FlightSchedule;
+import com.binar.flyket.model.*;
 import com.binar.flyket.model.user.User;
 import com.binar.flyket.repository.*;
 import com.binar.flyket.utils.Constants;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class BookingServiceImpl implements BookingService {
 
-    private final SeatRepository seatRepository;
+    private final AvailableSeatRepository availableSeatRepository;
     private final UserRepository userRepository;
     private final TicketRepository ticketRepository;
     private final PaymentMethodRepository paymentMethodRepository;
     private final FlightScheduleRepository flightScheduleRepository;
+    private final BookingRepository bookingRepository;
 
     public BookingServiceImpl(FlightScheduleRepository flightScheduleRepository,
                               UserRepository userRepository,
-                              SeatRepository seatRepository,
-                              TicketRepository ticketRepository,
-                              PaymentMethodRepository paymentMethodRepository) {
+                              AvailableSeatRepository availableSeatRepository, TicketRepository ticketRepository,
+                              PaymentMethodRepository paymentMethodRepository, BookingRepository bookingRepository) {
         this.flightScheduleRepository = flightScheduleRepository;
         this.userRepository = userRepository;
-        this.seatRepository = seatRepository;
+        this.availableSeatRepository = availableSeatRepository;
         this.ticketRepository = ticketRepository;
         this.paymentMethodRepository = paymentMethodRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     @Override
@@ -45,19 +46,36 @@ public class BookingServiceImpl implements BookingService {
         if(schedule.isEmpty())
             throw new FlyketException.EntityNotFoundException(HttpStatus.NOT_FOUND, "Flight Schedule with id " + Constants.NOT_FOUND_MSG);
 
-        return null;
+
+        String bookingId = "bk-" + UUID.randomUUID().toString().split("-")[0].toUpperCase();
+        Booking booking = new Booking();
+        booking.setId(bookingId);
+        booking.setExpiredTime(System.currentTimeMillis() + 1800000L);
+        booking.setAmount(request.getAmount());
+        booking.setCreatedAt(LocalDateTime.now());
+        booking.setUpdatedAt(LocalDateTime.now());
+        booking.setFlightSchedule(schedule.get());
+        booking.setBookingStatus(BookingStatus.ACTIVE);
+
+        bookingRepository.save(booking);
+
+        for(PassengerRequest passengerRequest : request.getPassengerRequests()) {
+            passengerTicket(passengerRequest, bookingId);
+        }
+
+        BookingResponse bookingResponse = new BookingResponse();
+        bookingResponse.setBookingId(bookingId);
+        bookingResponse.setBookingStatus(BookingStatus.ACTIVE);
+        bookingResponse.setDate(LocalDateTime.now());
+        bookingResponse.setName(user.get().getFirstName() + " " + user.get().getLastName());
+        bookingResponse.setUserEmail(user.get().getEmail());
+
+        return bookingResponse;
     }
 
-    private void insertPassenger(BookingRequest request, FlightSchedule flightSchedule) {
-        String uid = UUID.randomUUID().toString();
-        AircraftDetail aircraftDetail = flightSchedule.getAircraftDetail();
+    private void passengerTicket(PassengerRequest passengerRequest, String bookingId) {
 
     }
-
-    private Boolean isSeatAvailable(FlightSchedule flightSchedule) {
-return null;
-    }
-
 
     @Override
     public Boolean validateBooking(String userId, String bookingId) {
