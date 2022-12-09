7583f7978ba23a793c4da16906e8d741057ccecc
Agam33 <riswanagam@gmail.com>
2022-12-09 16:09:33 +0700

    Update TicketRepository.java

--- src/main/java/com/binar/flyket/repository/TicketRepository.java
+++ src/main/java/com/binar/flyket/repository/TicketRepository.java
@@ -1,16 +1,17 @@
 package com.binar.flyket.repository;
 
+import com.binar.flyket.dto.model.AvailableSeatDTO;
 import com.binar.flyket.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, String> {
 
-    @Query(value = "SELECT tc FROM Ticket tc WHERE tc.flightSchedule.id =:schedule_id AND tc.availableSeat.id =:avs_id")
-    List<Ticket> findAllTicketByScheduleAndAvsId(@Param("schedule_id") String scheduleId,
-                                         @Param("avs_id") Integer avsId);
+    @Query(value = "SELECT tc FROM Ticket AS tc ")
+   Optional<Ticket> findAvailableTicket(@Param("schedule_id") String scheduleId);
+
 }
