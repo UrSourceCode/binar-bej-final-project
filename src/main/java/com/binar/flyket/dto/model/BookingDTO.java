3cc42df3253b89af239ddad733b7dc6ab7d56df8
Agam33 <riswanagam@gmail.com>
2022-12-10 21:35:12 +0700

    Update BookingDTO.java

--- src/main/java/com/binar/flyket/dto/model/BookingDTO.java
+++ src/main/java/com/binar/flyket/dto/model/BookingDTO.java
@@ -1,11 +1,25 @@
 package com.binar.flyket.dto.model;
 
+import com.binar.flyket.model.BookingStatus;
+import com.binar.flyket.model.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @AllArgsConstructor
 public class BookingDTO {
+    private String userId;
+    private String email;
+    private String phoneNumber;
+    private String bookingId;
+    private String paymentMethod;
+    private BigDecimal amount;
+    private BookingStatus bookingStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
 }
