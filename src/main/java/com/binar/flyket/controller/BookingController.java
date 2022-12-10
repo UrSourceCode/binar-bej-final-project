491e26a7cc7453e5011c57ebc366dbb54a24eb48
Agam33 <riswanagam@gmail.com>
2022-12-10 10:38:35 +0700

    Update BookingController.java

--- src/main/java/com/binar/flyket/controller/BookingController.java
+++ src/main/java/com/binar/flyket/controller/BookingController.java
@@ -1,6 +1,7 @@
 package com.binar.flyket.controller;
 
 import com.binar.flyket.dto.request.BookingRequest;
+import com.binar.flyket.dto.request.PaymentRequest;
 import com.binar.flyket.dto.response.Response;
 import com.binar.flyket.dto.response.ResponseError;
 import com.binar.flyket.exception.FlyketException;
@@ -9,6 +10,8 @@ import com.binar.flyket.utils.Constants;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -24,12 +27,24 @@ public class BookingController {
         this.bookingService = bookingService;
     }
 
+    @PreAuthorize("hasRole('BUYER')")
     @PostMapping("/booking/add")
     public ResponseEntity<?> addBooking(@RequestParam("uid") String userId,
                                         @RequestBody BookingRequest bookingRequest) {
         try {
              return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(),
                      new Date(), Constants.SUCCESS_MSG, bookingService.addBooking(userId, bookingRequest)));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(), new Date(), e.getMessage()), e.getStatusCode());
+        }
+    }
+
+    @PreAuthorize("hasRole('BUYER')")
+    @PostMapping("/booking/set-payment")
+    public ResponseEntity<?> setPayment(@RequestBody PaymentRequest request) {
+        try {
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(),
+                    new Date(), Constants.SUCCESS_MSG, bookingService.setPaymentMethod(request)));
         } catch (FlyketException.EntityNotFoundException e) {
             return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(), new Date(), e.getMessage()), e.getStatusCode());
         }
