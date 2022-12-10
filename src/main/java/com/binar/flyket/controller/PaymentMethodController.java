05e00bb2efe1ea52aac9905ec606a361b2c2a364
Agam33 <riswanagam@gmail.com>
2022-12-10 10:12:03 +0700

    Create PaymentMethodController.java

---
+++ src/main/java/com/binar/flyket/controller/PaymentMethodController.java
@@ -0,0 +1,53 @@
+package com.binar.flyket.controller;
+
+
+import com.binar.flyket.dto.request.PaymentMethodRequest;
+import com.binar.flyket.dto.response.Response;
+import com.binar.flyket.dto.response.ResponseError;
+import com.binar.flyket.exception.FlyketException;
+import com.binar.flyket.service.PaymentMethodService;
+import com.binar.flyket.utils.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentMethodController {
+
+    private PaymentMethodService paymentMethodService;
+
+    public PaymentMethodController(PaymentMethodService paymentMethodService) {
+        this.paymentMethodService = paymentMethodService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestBody PaymentMethodRequest request) {
+        try {
+            paymentMethodService.addPaymentMethod(request);
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(), Constants.SUCCESS_MSG, null));
+        } catch (FlyketException.DuplicateEntityException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(), new Date(), e.getMessage()), e.getStatusCode());
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam("id") String id) {
+        try {
+            paymentMethodService.deletePaymentMethod(id);
+            return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), new Date(), Constants.SUCCESS_MSG, null));
+        } catch (FlyketException.EntityNotFoundException e) {
+            return new ResponseEntity<>(new ResponseError(e.getStatusCode().value(), new Date(), e.getMessage()), e.getStatusCode());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(),
+                new Date(),
+                Constants.SUCCESS_MSG, 
+                paymentMethodService.getAll()));
+    }
+}
