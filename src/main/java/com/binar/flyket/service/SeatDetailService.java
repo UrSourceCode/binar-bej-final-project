ecea9d40767f5fa45c4bdd8952e411c518e88a19
Agam33 <riswanagam@gmail.com>
2022-12-10 13:53:33 +0700

    Update SeatDetailService.java

--- src/main/java/com/binar/flyket/service/SeatDetailService.java
+++ src/main/java/com/binar/flyket/service/SeatDetailService.java
@@ -2,11 +2,16 @@ package com.binar.flyket.service;
 
 
 import com.binar.flyket.dto.request.SeatRequest;
+import com.binar.flyket.model.SeatDetail;
+
+import java.util.List;
 
 
 public interface SeatDetailService {
 
     Boolean addSeat(SeatRequest request);
+
+    List<SeatDetail> getAll();
 
 
 
