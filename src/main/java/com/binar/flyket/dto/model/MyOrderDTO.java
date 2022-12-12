c00c696e54f51b24ec8ce73152086db246b9e3a0
Agam33 <riswanagam@gmail.com>
2022-12-12 19:46:47 +0700

    Create MyOrderDTO.java

---
+++ src/main/java/com/binar/flyket/dto/model/MyOrderDTO.java
@@ -0,0 +1,30 @@
+package com.binar.flyket.dto.model;
+
+
+import com.binar.flyket.model.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyOrderDTO {
+    private String flightScheduleId;
+    private String bookingId;
+    private Integer totalPassenger;
+    private LocalDateTime arrivalTime;
+    private LocalDateTime departureTime;
+    private String originAirport;
+    private String destinationAirport;
+    private LocalDateTime purchaseCompleteAt;
+    private BookingStatus bookingStatus;
+    private Integer hours;
+    private Integer minutes;
+    private BigDecimal price;
+}
