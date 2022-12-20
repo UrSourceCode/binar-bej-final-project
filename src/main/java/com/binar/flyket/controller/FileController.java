936bd471aee2c3867347cd551454df3a06fbf4b7
Agam33 <riswanagam@gmail.com>
2022-12-20 19:29:35 +0700

    Update FileController.java

--- src/main/java/com/binar/flyket/controller/FileController.java
+++ src/main/java/com/binar/flyket/controller/FileController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -30,6 +31,7 @@ public class FileController {
         this.fileService = fileService;
     }
 
+    @PreAuthorize("hasRole('BUYER') or hasRole('ADMIN')")
     @GetMapping(value = "/download/invoice")
     public ResponseEntity<?> downloadInvoice(
             @RequestParam("user-id") String userId,
