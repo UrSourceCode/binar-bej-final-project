1f4f75364f90badaab94e8ca594de0ba63b0845b
Agam33 <riswanagam@gmail.com>
2022-12-21 23:31:29 +0700

    Update HistoryController.java

--- src/main/java/com/binar/flyket/controller/HistoryController.java
+++ src/main/java/com/binar/flyket/controller/HistoryController.java
@@ -3,17 +3,22 @@ package com.binar.flyket.controller;
 import com.binar.flyket.dto.response.Response;
 import com.binar.flyket.dto.response.ResponseError;
 import com.binar.flyket.exception.FlyketException;
+import com.binar.flyket.model.BookingStatus;
 import com.binar.flyket.service.TransactionHistoryService;
 import com.binar.flyket.utils.Constants;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @CrossOrigin(value = "*")
 @RestController
@@ -56,9 +61,27 @@ public class HistoryController {
     @GetMapping("/booking/get-all")
     public ResponseEntity<?> getAll(
             @RequestParam(defaultValue = "0", value = "page") int page,
-            @RequestParam(defaultValue = "10", value = "size") int size) {
-        Pageable paging = PageRequest.of(page, size, Sort.by("createdAt").descending());
+            @RequestParam(defaultValue = "10", value = "size") int size,
+            @RequestParam(defaultValue = "asc", value = "booking-status-sort") String bookingStatus,
+            @RequestParam(defaultValue = "asc", value = "booking-date-sort") String bookingDate) {
+
+        List<Sort.Order> orders = new ArrayList<>();
+
+        orders.add(new Sort.Order(getSortDirection(bookingStatus), "bookingStatus"));
+        orders.add(new Sort.Order(getSortDirection(bookingDate), "createdAt"));
+
+        Pageable paging = PageRequest.of(page, size, Sort.by(orders));
         return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(),
                 new Date(), Constants.SUCCESS_MSG, transactionHistoryService.getAllBookingHistory(paging)));
+    }
+
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
     }
 }
