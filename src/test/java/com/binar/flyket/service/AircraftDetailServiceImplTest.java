3cc0a3ae46c564b9535bc98ec5a013ac0ce3a908
Agam33 <riswanagam@gmail.com>
2022-12-24 23:27:37 +0700

    Create AircraftDetailServiceImplTest.java

---
+++ src/test/java/com/binar/flyket/service/AircraftDetailServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.binar.flyket.service;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AircraftDetailServiceImplTest {
+
+
+    @InjectMocks
+    private AircraftDetailServiceImpl aircraftDetailService;
+
+}
