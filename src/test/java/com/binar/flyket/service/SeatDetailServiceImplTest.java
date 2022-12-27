d19c1b0a8d92c712ae8d3e8bd48f5e9aab600648
Agam33 <riswanagam@gmail.com>
2022-12-27 14:13:50 +0700

    Update SeatDetailServiceImplTest.java

--- src/test/java/com/binar/flyket/service/SeatDetailServiceImplTest.java
+++ src/test/java/com/binar/flyket/service/SeatDetailServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.binar.flyket.service;
 
 import com.binar.flyket.dto.request.SeatRequest;
+import com.binar.flyket.dummy.SeatDetailDummies;
 import com.binar.flyket.repository.SeatDetailRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -16,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SeatDetailServiceImplTest {
 
     @InjectMocks
-    private SeatDetailService seatDetailService;
+    private SeatDetailServiceImpl seatDetailService;
 
     @Mock
     private SeatDetailRepository seatDetailRepository;
@@ -34,10 +36,21 @@ class SeatDetailServiceImplTest {
         Mockito.when(seatDetailRepository.findById(request.getSeatNo()))
                 .thenReturn(Optional.empty());
 
+        var actualValue = seatDetailService.addSeat(request);
+        var expectedValue = true;
+
+        Assertions.assertEquals(expectedValue, actualValue);
     }
 
     @Test
     void testGetAll() {
 
+        Mockito.when(seatDetailRepository.findAll())
+                .thenReturn(SeatDetailDummies.seatDetails());
+
+        var actualValue = seatDetailService.getAll();
+        var expectedSize = 3;
+
+        Assertions.assertEquals(expectedSize, actualValue.size());
     }
 }
