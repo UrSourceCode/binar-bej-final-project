2081ac35984ec5a99f02dc18746421b23115b3f6
Agam33 <riswanagam@gmail.com>
2022-12-27 16:22:53 +0700

    Update FlightRouteDummies.java

--- src/test/java/com/binar/flyket/dummy/FlightRouteDummies.java
+++ src/test/java/com/binar/flyket/dummy/FlightRouteDummies.java
@@ -8,15 +8,24 @@ import java.util.List;
 public class FlightRouteDummies {
 
     public static List<FlightRoute> flightRoute() {
-        FlightRoute fr = new FlightRoute();
-        fr.setId("CGK-NRT");
-        fr.setFromAirport(AirportDummies.airports().get(0));
-        fr.setToAirport(AirportDummies.airports().get(1));
-        fr.setHours(4);
-        fr.setMinutes(10);
-        fr.setUpdatedAt(LocalDateTime.now());
-        fr.setCreatedAt(LocalDateTime.now());
+        FlightRoute fr1 = new FlightRoute();
+        fr1.setId("CGK-NRT");
+        fr1.setFromAirport(AirportDummies.airports().get(0));
+        fr1.setToAirport(AirportDummies.airports().get(1));
+        fr1.setHours(4);
+        fr1.setMinutes(10);
+        fr1.setUpdatedAt(LocalDateTime.now());
+        fr1.setCreatedAt(LocalDateTime.now());
 
-        return List.of(fr);
+        FlightRoute fr2 = new FlightRoute();
+        fr2.setId("NRT-CGK");
+        fr2.setFromAirport(AirportDummies.airports().get(1));
+        fr2.setToAirport(AirportDummies.airports().get(0));
+        fr2.setHours(4);
+        fr2.setMinutes(10);
+        fr2.setUpdatedAt(LocalDateTime.now());
+        fr2.setCreatedAt(LocalDateTime.now());
+
+        return List.of(fr1, fr2);
     }
 }
