11325269397e82a264cd935b94b6d574a7e16faa
Riswan Agam <riswanagam@gmail.com>
2022-12-28 09:48:43 +0700

    Create bookingtest

--- src/test/java/com/binar/flyket/dummy/SeatDetailDummies.java
+++ src/test/java/com/binar/flyket/dummy/SeatDetailDummies.java
@@ -3,6 +3,7 @@ package com.binar.flyket.dummy;
 import com.binar.flyket.model.SeatDetail;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SeatDetailDummies {
 
@@ -20,5 +21,11 @@ public class SeatDetailDummies {
         sd3.setId("1C");
 
         return List.of(sd1, sd2, sd3);
+    }
+
+    public static SeatDetail findSeatById(String seatId) {
+        return seatDetails()
+                .stream().filter(it -> Objects.equals(it.getId(), seatId))
+                .findFirst().get();
     }
 }
